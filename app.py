50f22120c307a98a14f3afdfc746db90b038b949
Luisarg03 <luis.m.paz.03@gmail.com>
2020-04-08 11:52:22 -0300

    Add chooise path

--- app.py
+++ app.py
@@ -2,24 +2,27 @@ import youtube_dl as ytd
 
 print('Conversos de videos a mp3.')
 
+path = input('Ingresa la ruta donde se guardaran los archivos: ')
 
 def proceso():
-    url = input('Url de video o lista de reproduccion: ')
-    ydl_opts = {
-        'outtmpl': 'D:\Musica\%(title)s-%(id)s.%(ext)s',
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',  # 'flac',
-            'preferredquality': '320',
-        }],
-    }
-    try:
-        with ytd.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-    except:
-        print('No se a podido completar la operacion.')
-        return salir()
+    while True:
+        url = input('Url de video o lista de reproduccion: ')
+        ydl_opts = {
+            'outtmpl': path + '\%(title)s-%(id)s.%(ext)s',
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',  # 'flac',
+                'preferredquality': '320',
+            }],
+        }
+        try:
+            with ytd.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+            salir()
+        except:
+            print('No se a podido completar la operacion.')
+            return salir()
 
 
 def salir():
